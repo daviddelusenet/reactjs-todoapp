9d4bbf71cd0af26d2e2dfce897342c6201bf5f9d
David de Lusenet <lusenet@momkai.com>
2017-04-21 16:20:58 +0200

    Added (not working jet) CSSTransitionGroup

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import styles from './Todo.scss';
 
 class Todo extends React.Component {
@@ -38,13 +39,31 @@ class Todo extends React.Component {
   }
 
   render() {
+
+    //<CSSTransitionGroup
+    //  transitionName={{
+    //      enter: styles['Todo--enter'],
+    //      enterActive: styles['Todo--enter-active'],
+    //      leave: styles['Todo--leave'],
+    //      leaveActive: styles['Todo--leave-active']
+    //    }}
+    //  transitionEnterTimeout={500}
+    //  transitionLeaveTimeout={300}>
+    //  <li styleName="Todo" ref={(li) => {this.todo = li}}>
+    //    <input styleName="Todo-checkbox" type="checkbox" ref={(checkbox) => {this.checkbox = checkbox}} onChange={this.handleChange} checked={this.props.todo.done} />
+    //    <input styleName="Todo-input" ref={(input) => {this.input = input}} defaultValue={this.props.todo.text} onKeyDown={this.handleUpdate}></input>
+    //    <span styleName="Todo-text" onDoubleClick={this.handleEdit}>{this.props.todo.text}</span>
+    //    <span styleName="Todo-delete" onClick={this.handleDelete}>&#10006;</span>
+    //  </li>
+    //</CSSTransitionGroup>
+
     return(
-        <li styleName="Todo" ref={(li) => {this.todo = li}}>
-          <input styleName="Todo-checkbox" type="checkbox" ref={(checkbox) => {this.checkbox = checkbox}} onChange={this.handleChange} checked={this.props.todo.done} />
-          <input styleName="Todo-input" ref={(input) => {this.input = input}} defaultValue={this.props.todo.text} onKeyDown={this.handleUpdate}></input>
-          <span styleName="Todo-text" onDoubleClick={this.handleEdit}>{this.props.todo.text}</span>
-          <span styleName="Todo-delete" onClick={this.handleDelete}>&#10006;</span>
-        </li>
+      <li styleName="Todo" ref={(li) => {this.todo = li}}>
+        <input styleName="Todo-checkbox" type="checkbox" ref={(checkbox) => {this.checkbox = checkbox}} onChange={this.handleChange} checked={this.props.todo.done} />
+        <input styleName="Todo-input" ref={(input) => {this.input = input}} defaultValue={this.props.todo.text} onKeyDown={this.handleUpdate}></input>
+        <span styleName="Todo-text" onDoubleClick={this.handleEdit}>{this.props.todo.text}</span>
+        <span styleName="Todo-delete" onClick={this.handleDelete}>&#10006;</span>
+      </li>
     );
   }
 
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -7,15 +7,15 @@ class TodoList extends React.Component {
 
   render() {
     return(
-        <ul styleName="TodoList">
-          {this.props.loading ? (
-              <TodosAreLoading />
-            ) : (
-              this.props.todos.map((object, key) =>
-                  <Todo key={key} id={key} deleteTodo={this.props.deleteTodo} toggleDone={this.props.toggleDone} updateTodo={this.props.updateTodo} todo={object} />
-              )
-          )}
-        </ul>
+      <ul styleName="TodoList">
+        {this.props.loading ? (
+            <TodosAreLoading />
+          ) : (
+            this.props.todos.map((object, key) =>
+              <Todo key={key} id={key} deleteTodo={this.props.deleteTodo} toggleDone={this.props.toggleDone} updateTodo={this.props.updateTodo} todo={object} />
+            )
+        )}
+      </ul>
     );
   }
 
